495c750a9f39af5c1cc3a29b75094cd13cc67c58
Riley Slayden <slaydenriley@gmail.com>
2020-08-17 15:35:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 class Formatter {
   static capitalize(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1)
-  }
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
 
   static sanitize( string ) {
-    return string.replace( /[^A-Za-z0-9 '-]/g, '' )
-  }
+    return string.replace( /[^A-Za-z0-9 '-]/g, '' );
+  };
 }
